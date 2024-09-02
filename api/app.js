af62cb6a6d30a155165736a0867231efabcccc4b
Cherevatyi Mykyta <nikita.cherevatyi@extern.a1.at>
2024-09-02 17:16:24 +0300

    Working dir

---
+++ api/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Home Route');
+});
+app.listen(port, () =>
+  console.log(`Server running on port ${port}, http://localhost:${port}`)
+);
